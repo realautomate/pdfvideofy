8747e130310e31096f89fbacfce0d604b3ca6f3d
realautomate <qtran1215@gmail.com>
2026-05-17 11:38:09 +0700

    fix: host tailwind locally and update ffmpeg variable namespace

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
 // Setup FFmpeg
-const { FFmpeg } = window.FFmpeg;
+const { FFmpeg } = window.FFmpegWASM;
 const { fetchFile } = window.FFmpegUtil;
 let ffmpeg = null;
 
